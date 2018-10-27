046457e95f7573584303a4bdb9a110dbfb771577
giovan herrera fontalvo <herrerafontalvo@outlook.com>
2018-10-26 21:28:39 -0500

    Creando la base de datos

--- app/src/main/java/com/example/gio10/pruebassaber11/MainActivity.java
+++ app/src/main/java/com/example/gio10/pruebassaber11/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.gio10.pruebassaber11;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +10,15 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.gio10.pruebassaber11.model.DaoEstudiante;
 import com.example.gio10.pruebassaber11.model.EstudianteDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText nombre, capital, nHab, idiom, moned;
+    EditText id, nombre, apellido, colegio, dpto, ciudad;
     Spinner tipoColeg, pLectCrit, pMat, pSoc, pNatur, pIngles;
-
     Button guardar, mostrar;
 
     EstudianteDbHelper basedatos;
@@ -80,5 +83,83 @@ public class MainActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         pIngles.setAdapter(adapterIng);
 
+        guardar = (Button) findViewById(R.id.btnGuardar);
+        mostrar = (Button) findViewById(R.id.btnMostrar);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardar();
+            }
+        });
+
+        mostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,ListaEstudiantes.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    public void abrir(){
+        try {
+            basedatos = new EstudianteDbHelper(this);
+        }
+        catch(Exception e){
+            Toast.makeText(this,"Error al crear base datos", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void close(){
+        try {
+            basedatos.close();
+        }
+        catch(Exception e){
+            Toast.makeText(this,"Error al cerrar base datos", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void guardar(){
+        abrir();
+        try {
+            SQLiteDatabase sqldata = basedatos.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            id = (EditText) findViewById(R.id.txtIdentificacion);
+            nombre = (EditText) findViewById(R.id.txtNombre);
+            apellido = (EditText) findViewById(R.id.txtApellido);
+            colegio = (EditText) findViewById(R.id.txtColegio);
+            dpto = (EditText) findViewById(R.id.txtDepartamento);
+            ciudad = (EditText) findViewById(R.id.txtCiudad);
+
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_IDENTIFICACION, id.getText().toString());
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_NOMBRE, nombre.getText().toString());
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_APELLIDO, apellido.getText().toString());
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_COLEGIO, colegio.getText().toString());
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_TCOLEGIO, tipoColeg.getSelectedItem().toString());
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_DEPARTAMENTO, dpto.getText().toString());
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_CIUDAD, ciudad.toString());
+            values.put(DaoEstudiante.EstudianteEntry.COLUMN_NAME_PUNTAJE, puntaje());
+
+            long newRowId = sqldata.insert(DaoEstudiante.EstudianteEntry.TABLE_NAME, null, values);
+            Toast.makeText(this,"INSERCIÔN EXITOSA", Toast.LENGTH_LONG).show();
+            close();
+        }
+        catch (Exception e){
+            Toast.makeText(this,"Error al insertar Datos", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public double puntaje(){
+        double punt = 0;
+        double puntajeLC = Double.parseDouble(pLectCrit.getSelectedItem().toString())*3;
+        double puntajeMat = Double.parseDouble(pMat.getSelectedItem().toString())*3;
+        double puntajeSoc = Double.parseDouble(pSoc.getSelectedItem().toString())*3;
+        double puntajeCienc = Double.parseDouble(pNatur.getSelectedItem().toString())*3;
+        double puntajeIng = Double.parseDouble(pIngles.getSelectedItem().toString());
+        punt = (puntajeLC + puntajeMat + puntajeSoc + puntajeCienc + puntajeIng)/13;
+        punt = punt*5;
+        return punt;
     }
 }
